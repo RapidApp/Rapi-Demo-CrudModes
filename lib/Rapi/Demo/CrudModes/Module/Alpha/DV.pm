1df7c9ef9f4e583bb53200c2556acf18baafd50d
Henry Van Styn <vanstyn@intellitree.com>
2015-06-04 22:25:14 -0400

    Convert to Role + front-side crud options dump

--- lib/Rapi/Demo/CrudModes/Module/Alpha/DV.pm
+++ lib/Rapi/Demo/CrudModes/Module/Alpha/DV.pm
@@ -5,31 +5,10 @@ use warnings;
 
 use Moose;
 extends 'RapidApp::Module::DbicDV';
+with 'Rapi::Demo::CrudModes::Module::Alpha::Role';
 
 use RapidApp::Util qw(:all);
 use Path::Class qw(file dir);
-
-has '+include_colspec', default => sub {['*']};
-has '+updatable_colspec', default => sub {['*']};
-has '+creatable_colspec', default => sub {['*']};
-has '+destroyable_relspec', default => sub {['*']};
-
-has '+use_add_form',  default => 0;
-has '+use_edit_form', default => 0;
-
-has '+persist_immediately', default => sub{{
-  create  => 1,
-  update  => 1,
-  destroy => 1
-}};
-
-has '+confirm_on_destroy', default => 0;
-
-has 'ResultSource', is => 'ro', lazy => 1, default => sub {
-  my $self = shift;
-  $self->app->model('DB')->schema->source('Alpha')
-};
-
 
 has '+tt_file', default => 'templates/alpha_dv.html';
 
@@ -44,7 +23,6 @@ sub BUILD {
   $self->apply_extconfig( 
     itemSelector  => 'div.alpha-row',
     selectedClass => 'selected',
-    show_store_button_text => \1
   );
 }
 
--- lib/Rapi/Demo/CrudModes/Module/Alpha/Grid.pm
+++ lib/Rapi/Demo/CrudModes/Module/Alpha/Grid.pm
@@ -5,37 +5,8 @@ use warnings;
 
 use Moose;
 extends 'RapidApp::Module::DbicGrid';
+with 'Rapi::Demo::CrudModes::Module::Alpha::Role';
 
-use RapidApp::Util qw(:all);
-
-has '+include_colspec', default => sub {['*']};
-has '+updatable_colspec', default => sub {['*']};
-has '+creatable_colspec', default => sub {['*']};
-has '+destroyable_relspec', default => sub {['*']};
-
-has '+use_add_form',  default => 0;
-has '+use_edit_form', default => 0;
-
-has '+persist_immediately', default => sub{{
-  create  => 1,
-  update  => 1,
-  destroy => 1
-}};
-
-has '+confirm_on_destroy', default => 0;
-
-has 'ResultSource', is => 'ro', lazy => 1, default => sub {
-  my $self = shift;
-  $self->app->model('DB')->schema->source('Alpha')
-};
-
-
-sub BUILD {
-  my $self = shift;
-  
-  # Show full text with the store buttons (default is false)
-  $self->apply_extconfig( show_store_button_text => \1 );
-}
 
 1;
 
